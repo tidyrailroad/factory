6969e04b0f4e1fa6ad18de4e8b7f7e99798e49d5
Emory <emory.merryman@gmail.com>
2016-12-19 00:39:13 +0000

    fixed one off error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 (cat > Dockerfile <<EOF
 FROM emorymerryman/base:0.0.0
 MAINTAINER Emory Merryman emory.merryman@gmail.com
-RUN ["/sbin/apk", "add", ${0}]
-ENTRYPOINT [${1}]
+RUN ["/sbin/apk", "add", ${1}]
+ENTRYPOINT [${2}]
 CMD []
 EOF
     ) &&
