df35ae27522e9f614d19c6e40f0ce5b2eb2ecbc9
Emory <emory.merryman@gmail.com>
2016-12-19 00:57:52 +0000

    fixed quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 (cat > Dockerfile <<EOF
 FROM emorymerryman/base:0.0.0
 MAINTAINER Emory Merryman emory.merryman@gmail.com
-RUN ["/sbin/apk", "add", ${1}]
+RUN ["/sbin/apk", "add", "${1}"]
 ENTRYPOINT [${2}]
 CMD []
 EOF
